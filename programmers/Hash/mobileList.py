65f94ff53fa6844ca54c2377a55fb890d22ba91c
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-02-17 19:51:35 +0900

    make dir

--- programmers/Hash/mobileList.py
+++ programmers/Hash/mobileList.py
@@ -6,16 +6,18 @@ def solution(phoneBook):
             return False
     return True
 
-#이해덜 됨
-def solution(phone_book):
-    answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if len(phone_book[i]) < len(phone_book[i+1]):
-            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
-                answer = False
-                break
-    return answer
+
+# #이해덜 됨
+
+# def solution(phone_book):
+#     answer = True
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1):
+#         if len(phone_book[i]) < len(phone_book[i+1]):
+#             if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
+#                 answer = False
+#                 break
+#     return answer
 
 
 # phoneBook = ["119", "97674223", "1195524421"]
