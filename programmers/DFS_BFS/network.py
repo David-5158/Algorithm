7ae73e832ce9ce61423ea95addd36f0489a4292d
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-03-17 20:53:46 +0900

    DFS network algo

--- programmers/DFS_BFS/network.py
+++ programmers/DFS_BFS/network.py
@@ -1,35 +1,22 @@
-from collections import deque
-
 def solution(n, computers):            
     
-    def BFS(i):
-        q = deque()
-        q.append(i)
-        
-        print("q-->", q)
-        while q:
-            i = q.popleft()
-            print("i-->",i)
-            visited[i] = 1
-            
-            print("visited-->", visited)
-            for a in range(n):
-                if computers[i][a] and not visited[a]:
-                    print("if",computers[i][a],"and not",visited[a])
-                    q.append(a)
-                    print(f"q.append({a})")
+    def DFS(i):
+        visited[i] = 1
+        print(visited)
+        for a in range(n):
+            print("DFS for문")
+            if computers[i][a] and not visited[a]:
+                print("DFS if문")
+                DFS(a)
+                print("DFS 재귀문")
                 
     answer = 0
     visited = [0 for i in range(len(computers))]
-    
     for i in range(n):
         if not visited[i]:
-            BFS(i)
+            DFS(i)
             answer += 1
-            print(answer)
         
     return answer
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
-
-## DFS / BFS 확실히 이해하기 !!!!
+print(solution(3,[[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
